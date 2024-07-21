296ce4ec602e44aab8af7da70abacc1ce369aa29
OSaMa <166417491+Osama1010C@users.noreply.github.com>
2024-07-21 18:53:14 +0300

    Add files via upload

---
+++ Grouping/Grouping/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace Grouping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            
+            
+
+        }
+        public static void Menu()
+        {
+            
+            string header = "==========================\n     Qualifying Stage\n==========================\n";
+            
+            while (true)
+            {
+                int select;
+                Console.WriteLine(header);
+                Console.WriteLine("1] Start The Stage\n\n2] Exit\n");
+                while (true)
+                {
+                    try
+                    {
+                        select = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unknown Choice");
+                        continue;
+                    }
+                    if (select > 0 && select < 3)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unknown Choice");
+                        continue;
+                    }
+                }
+                if (select == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine(header);
+                    
+
+
+                    int numberOfUsers;
+                    while (true)
+                    {
+                        Console.Write("Enter Number Of Users : ");
+                        try
+                        {
+                            numberOfUsers = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Invalid Input");
+                            continue;
+                        }
+                        if (numberOfUsers > 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please Enter Number Greater Than Zero");
+                            continue;
+                        }
+                    }
+
+
+                    
+                    string[] arr = new string[numberOfUsers];
+                    Console.Clear();
+                    Console.WriteLine(header);
+                    Console.WriteLine("\nEnter Names Of Users \n-----------------------\n");
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        Console.Write($"User ({i+1}) : ");
+                        arr[i] = Console.ReadLine();
+                    }
+                    Console.Clear();
+                    Console.WriteLine(header);
+                    Stage s = new Stage(numberOfUsers);
+                    s.DisplayMatchesCenimatic(arr);
+                    Console.WriteLine("====================================\n< Click Enter To Back >");
+                    string enter = Console.ReadLine();
+                    Console.Clear();
+                }
+                else break;
+
+            }
+            
+        }
+    }
+}
---
+++ Grouping/Grouping/Stage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grouping
+{
+    internal class Stage
+    {
+        private List<string> UsersNames = new List<string>();
+
+        private int usersNum;
+        public int UsersNum
+        {
+            get
+            {
+                return this.usersNum;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    this.usersNum = 0;
+                }
+                else if(value % 2 != 0)
+                {
+                    this.usersNum = value + 1;
+                    UsersNames.Add("NO ONE");
+                }
+                else
+                {
+                    this.usersNum = value;
+                }
+            }
+        }
+
+        
+        public Stage(int usernum)
+        {
+            UsersNum = usernum;
+        }
+        public Stage() : this(0)
+        {
+
+        }
+        
+        private void AddUsers(string[] users)
+        {
+            foreach(string user in users)
+            {
+                UsersNames.Add(user);
+            }
+        }
+
+        private string[] GenerateStage(string[] users)
+        {
+            string[] result = new string[UsersNum];
+            AddUsers(users);
+            Random random = new Random();
+            int iterator = 0;
+            while(UsersNames.Count() != 0)
+            {
+                int randomNum = random.Next(0, UsersNames.Count());
+                result[iterator] = UsersNames[randomNum];
+                UsersNames.RemoveAt(randomNum);
+                iterator++;
+            }
+            return result;
+        }
+
+        public void DisplayMatchesCenimatic(string[] users)
+        {
+            string[] result = GenerateStage(users);
+            
+            for(int i = 0; i < UsersNum-1; i+=2)
+            {
+                Console.Write(result[i]);
+                Thread.Sleep(1000);
+                Console.Write(" VS ");
+                Thread.Sleep(1000);
+                Console.WriteLine(result[i+1] + "\n");
+                Thread.Sleep(1500);
+            }           
+        }
+        public void DisplayMatchesQuickly(string[] users)
+        {
+            string[] result = GenerateStage(users);
+
+            for (int i = 0; i < UsersNum - 1; i += 2)
+            {
+                Console.Write(result[i]);
+                
+                Console.Write(" VS ");
+                
+                Console.WriteLine(result[i + 1] + "\n");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
